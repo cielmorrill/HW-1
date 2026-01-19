96b59244fa2a6bae6563f0875a1a4d90efa4b066
cielmorrill <cicimorrill@gmail.com>
2026-01-19 14:55:03 -0500

    part 1 completed

--- hw1p1.hs
+++ hw1p1.hs
@@ -8,7 +8,6 @@
 -- :l hw1p1.hs (lowercase L, not 1)
 -- main
 
-func a b = a + b
 
 -- sumUp sums a list of numbers
 sumUp :: [Int] -> Int
@@ -17,22 +16,33 @@ sumUp (x:xs) = x + sumUp xs
 
 -- evens returns a list of the even numbers from the input list
 evens :: [Int] -> [Int]
-evens []     = undefined
-evens (x:xs) = undefined
+evens []     = []
+evens (x:xs)
+  | x `mod` 2 == 0 = x : evens xs
+  | otherwise    = evens xs
 
 -- incAll increments each number in the input list by 1
 incAll :: [Int] -> [Int]
-incAll = undefined
+incAll [] = []
+incAll (x:xs) = (x + 1) : incAll xs
 
 -- incBy increments each number in the input list by the given amount
 incBy :: Int -> [Int] -> [Int]
-incBy = undefined
+incBy n [] = []
+incBy n (x:xs) = (x + n) : incBy n xs
 
 -- append concatenates two lists (cannot use ++ operator)
 append :: [Int] -> [Int] -> [Int]
-append = undefined
+append [] ys = ys
+append (x:xs) ys = x : append xs ys
 
 main :: IO ()
 main = do
   putStrLn "Hello, World!"
-  print $ func 3 5
+  print $ sumUp [1,2,3,4,5]
+  print $ sumUp []
+  print $ evens [1,2,3,4,5,6]
+  print $ evens [1,3,5]
+  print $ incAll [1,2,3]
+  print $ incBy 5 [1,2,3]
+  print $ append [1,2,3] [4,5,6]
