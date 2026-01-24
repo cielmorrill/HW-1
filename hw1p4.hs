175548f7623bb2ec52862b7c1c4821b86c6f968f
cielmorrill <cicimorrill@gmail.com>
2026-01-24 14:40:02 -0500

    updating

--- hw1p4.hs
+++ hw1p4.hs
@@ -8,13 +8,31 @@
 -- :l hw1p4.hs (lowercase L, not 1)
 -- main
 
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe
+
+type Node = String
+type DAG = Map.Map Node (Set.Set Node)
+
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = "e"
+
+g = Map.fromList [(a, Set.fromList [b,c]),
+                  (b, Set.fromList [d]),
+                  (c, Set.fromList [d]),
+                  (d, Set.fromList []),
+                  (e, Set.fromList [a, c])]
 
 -- define each of the functions defined in Problem 1, but using higher-order functions built into the Prelude.
 -- In particular: map, filter, and the two folds, foldr and foldl. 
 
 -- sumUp returns the sum of all numbers in the input list
 sumUp' :: [Int] -> Int
-sumUp' l = undefined
+sumUp' l = map (+ xs) [x:xs]
 
 -- evens returns a list of the even numbers from the input list
 evens' :: [Int] -> [Int]
